427d21e3bdfe1f20878939413827004740efb730
mgaspari <marcoanthonygaspari@gmail.com>
2017-10-10 10:30:01 -0400

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,22 @@
 // Code CoordinatesButton Component Here
+import React from "react";
+
+export default class CoordinatesButton extends React.Component{
+  // constructor(props){
+  //   super()
+  //   this.state = {
+  //
+  //   }
+  // }
+    getCord = (e) => {
+      var cords = [e.clientX, e.clientY]
+      console.log(cords)
+      this.props.onReceiveCoordinates(cords)
+    }
+
+  render(){
+    return(
+      <button onClick={this.getCord}>Hi there!</button>
+    )
+  }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,15 @@
 // Code DelayedButton Component Here
+import React from "react";
+
+export default class DelayedButton extends React.Component {
+  dlyfun = (e) => {
+    e.persist()
+    setTimeout(() => this.props.onDelayedClick(e), this.props.delay)
+
+  }
+  render(){
+    return(
+      <button onClick={this.dlyfun}>Click here too!</button>
+    )
+  }
+}
